efb78a8e87c217b16b061832663c2521ce1251a5
ddom <david.domask@veeam.com>
2020-07-23 00:36:36 +0300

    Added configuration values at start of the script.

--- tapemonitor.ps1
+++ tapemonitor.ps1
@@ -6,20 +6,25 @@ Editing to check for drives is simple, just change the gwmi query to fetch drive
 gwmi -Class Win32_TapeDrive
 #>
 
-for($i=0;$i -lt '60480'; $i++){
+#Configuration variables#
+
+$logpath = "C:\temp"
+$ChangerNum = "2"
+
+for($i=0;$i -lt "60480"; $i++){
 $params = gwmi -Namespace root\wmi -Class mschangerparameters
-if (!($params.Length -eq '2')){
-	add-content -path 'C:\temp\tapemonitor.log'	-value (Get-Date)
-	add-content -path 'C:\temp\tapemonitor.log' -value "$($params.InstanceName)"
-	add-content -path 'C:\temp\tapemonitor.log' -value "This is the only connected Library"
-	add-content -path 'C:\temp\tapemonitor.log' -value "Windows was not able to see the other connected device"
-    add-content -path 'C:\temp\tapemonitor.log' -value "The script will now terminate, please provide the log to Veeam"
-    explorer.exe "C:\temp"
+if (!($params.Length -eq "$ChangerNum")){
+	add-content -path "$logpath\tapemonitor.log" -value (Get-Date)
+	add-content -path "$logpath\tapemonitor.log" -value "$($params.InstanceName)"
+	add-content -path "$logpath\tapemonitor.log" -value "This is the only connected Library"
+	add-content -path "$logpath\tapemonitor.log" -value "Windows was not able to see the other connected device"
+    add-content -path "$logpath\tapemonitor.log" -value "The script will now terminate, please provide the log to Veeam"
+    explorer.exe "$logpath"
     exit
 	}
-	elseif($i % 6 -eq '0'){
-		add-content -path 'C:\temp\tapemonitor.log'	-value (Get-Date)
-		add-content -path 'C:\temp\tapemonitor.log'	-value "Both Libraries Connected"
+	elseif($i % 6 -eq "0"){
+		add-content -path "$logpath\tapemonitor.log"	-value (Get-Date)
+		add-content -path "$logpath\tapemonitor.log"	-value "Both Libraries Connected"
 		Start-sleep 10
 		}
     else {
