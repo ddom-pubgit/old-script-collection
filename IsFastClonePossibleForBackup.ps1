4a878fcab85880eb37502d1a245f5b324a72be31
ddom <david.domask@veeam.com>
2020-11-20 18:10:04 +0300

    Added function Get-RepositoryTypeAndInfo to clean up the logic for the core loop. Will be easier to add other types of Fast-Clone possible repositories in the future, and to validate for non-Fast-Clone possible repositories.
    Additional cleaning suggestions per Sergey S finally implemented.

--- IsFastClonePossibleForBackup.ps1
+++ IsFastClonePossibleForBackup.ps1
@@ -1,6 +1,13 @@
 <# script returns the blocksize of Storages for a Backup and also the Repository Information.
 Works for Scaleout Repos and also Local Windows/Linux (should work for XFS)
-Not Tested with CIFS or CloudConnect Repos
+Not Tested with CloudConnect Repos
+Currently breaks with Agent backups (all types) when returning storages, will be fixed in next version
+#>
+
+<#Todo:
+
+- Add validation rules for EpAgentBackups (not returned by Get-VBRRestorepoint)
+- Add Validation check on SOBR for non-FastClone Possible repositories, and and include Repo field for storages
 #>
 
 param (
@@ -25,6 +32,14 @@ function Get-LinuxRepositoryInfo {
 		return $LinuxRepositoryInfo
 	}
 
+function Get-CIFSRepositoryInfo {
+	param(
+		[string[]]$RepositoryID
+		)
+		$CIFSRepositoryInfo = [Veeam.Backup.Core.CCifsRepository]::FindByRepository("$RepositoryID") 
+		return $CIFSRepositoryInfo
+	}
+
 function Get-ExtentFromScaleout {
 	param(
 		[string[]]$ScaleoutRepoName
@@ -41,45 +56,43 @@ function Show-IsFastClonePossibleandClusterSize {
 		$return = [ordered]@{
 			RepositoryID = $BackupRepositoryInfo.RepositoryID
 			FastClonePossible = ($BackupRepositoryInfo.IsVirtualSyntheticEnabled -and $BackupRepositoryInfo.IsVirtualSyntheticAvailable)
-        	RepoClusterSize = $BackupRepositoryInfo.ClusterSize
+			RepoClusterSize = $BackupRepositoryInfo.ClusterSize
 			IsDedupEnabledWinOnly = $BackupRepositoryInfo.IsDedupEnabled
 		}
 		return $return
 	}
 
+function Get-RepositoryTypeAndInfo {
+	param(
+		[Object[]]$RepositoryToCheck
+	)
+	If($RepositoryToCheck.Type -eq "LinuxLocal"){
+		$RepoInfo = Get-LinuxRepositoryInfo -RepositoryID $RepositoryToCheck.id
+	} elseif ($RepositoryToCheck.Type -eq "CifsShare") {
+		$RepoInfo = Get-CIFSRepositoryInfo -RepositoryID $RepositoryToCheck.id
+	} elseif ($RepositoryToCheck.Type -eq "WinLocal"){
+		$RepoInfo = Get-WindowsRepositoryInfo -RepositoryID $RepositoryToCheck.id
+	}
+	$Repositories += (Show-IsFastClonePossibleandClusterSize -BackupRepositoryInfo $RepoInfo)
+	return	$Repositories
+} 	
+
 $Backup = Get-VBRBackup -Name $BackupName
 $RestorePoints = Get-VBRRestorePoint -Backup $Backup
-$TargetRepository = $Backup.FindRepository()[0] #Jobs with Offloads will return multiple Repository Objects from FindRepository() but both will be PerformanceTier. Safe to simply set the first
+$RepositoryToCheck = $Backup.FindRepository()[0] #Jobs with Offloads in Copy Mode will return multiple entries from FindRepository() but both will be PerformanceTier. Safe to simply set the first
 $StoragesStats = $RestorePoints.FindStorage() |Sort-Object -Property CreationTime -Descending |Select-Object -Property Partialpath,BlockAlignmentSize
 $Repositories = @()
 
-If($TargetRepository.Type -eq 'ExtendableRepository'){
-		$ExtentList = Get-ExtentFromScaleout -ScaleoutRepoName $TargetRepository.Name
-		Foreach($Extent in $ExtentList){
-			If($Extent.Type -eq "LinuxLocal"){
-				$LinuxRepo = Get-LinuxRepositoryInfo -RepositoryID $Extent.id
-				$RepositoryResult = Show-IsFastClonePossibleandClusterSize -BackupRepositoryInfo $LinuxRepo
-				$Repositories += $RepositoryResult
-			} else  {
-				$WindowsRepo = Get-WindowsRepositoryInfo -RepositoryID $Extent.id
-				$RepositoryResult = Show-IsFastClonePossibleandClusterSize -BackupRepositoryInfo $WindowsRepo
-				$Repositories += $RepositoryResult
-			}
+If($RepositoryToCheck.Type -eq 'ExtendableRepository'){
+		$ExtentList = Get-ExtentFromScaleout -ScaleoutRepoName $RepositoryToCheck.Name
+		Foreach($RepositoryToCheck in $ExtentList){
+			Get-RepositoryTypeAndInfo -RepositoryToCheck $RepositoryToCheck
 		}
 	$Repositories | Out-Host
 	$StoragesStats | Out-Host
-}
-
-If($TargetRepository.Type -eq "WinLocal" -or $TargetRepository.Type -eq "LinuxLocal"){
-	If($TargetRepository.Type -eq "LinuxLocal"){
-		$LinuxRepo = Get-LinuxRepositoryInfo -RepositoryID $TargetRepository.id
-		$RepositoryResult = Show-IsFastClonePossibleandClusterSize -BackupRepositoryInfo $LinuxRepo
-		$Repositories += $RepositoryResult
-	} else  {
-		$WindowsRepo = Get-WindowsRepositoryInfo -RepositoryID $TargetRepository.id
-		$RepositoryResult = Show-IsFastClonePossibleandClusterSize -BackupRepositoryInfo $WindowsRepo
-		$Repositories += $RepositoryResult
-	}
+} else {
+	Get-RepositoryTypeAndInfo -RepositoryToCheck $RepositoryToCheck
 	$Repositories | Out-Host
 	$StoragesStats | Out-Host
 }
+	
