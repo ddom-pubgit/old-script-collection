e614568d2a2cf96d86837d87dbec3ea9f80995c9
ddom <david.domask@veeam.com>
2020-11-30 11:43:34 +0300

    Small Code Cleanup to include nicer functions and automatically handle Changers vs Drives

--- tapemonitor.ps1
+++ tapemonitor.ps1
@@ -1,31 +1,46 @@
-<# A very simple script to monitor the connected tape changers. 
-If the count of expected changers drops below the set number, we write to the log and terminate the script.
+<# A very simple script to monitor the connected tape devices. 
+If the count of expected devices drops below the set number, we write to the log and terminate the script.
 The script checks every 10 seconds for the connected devices.
-Every minute, it will log if both devices are successfully connected.
-Editing to check for drives is simple, just change the gwmi query to fetch drives:
-gwmi -Class Win32_TapeDrive
+Every minute, it will log if All devices are successfully connected.
 #>
 
 #Configuration variables. Adjust these as necessary for the environment
 
-$logpath = "C:\temp"
-$ChangerNum = "2" #Confirm how many should be connected to the tape server
+$logpath = "C:\temp\TapeMonitorLog.log"
 $Duration = "60480" #Value should be (total duration in seconds)/10. Default is 1 week. The script should not be aggressive, so feel free to run it for weeks even
 
+function Write-Log {
+    param(
+        [string[]]$LogText
+        )
+	$logentry = "$(Get-Date) ::: "	
+    Add-Content -Path $logpath -Value "$logentry $LogText"
+    }
+
+function Get-DeviceCount {
+	$Changers = gwmi -Namespace root\wmi -Class mschangerparameters
+	if(!$Changers){
+		$Drives = gwmi -Class Win32_TapeDrive
+		$DeviceNum = $Drives.Count
+	} else {
+		$DeviceNum = $Changers.Count
+	}
+return $DeviceNum
+}
+
+$WorkingCount = Get-DeviceCount
+
 for($i=0;$i -lt "$Duration"; $i++){
-$params = gwmi -Namespace root\wmi -Class mschangerparameters
-if (!($params.Length -eq "$ChangerNum")){
-	add-content -path "$logpath\tapemonitor.log" -value (Get-Date)
-	add-content -path "$logpath\tapemonitor.log" -value "$($params.InstanceName)"
-	add-content -path "$logpath\tapemonitor.log" -value "This is the only connected Library"
-	add-content -path "$logpath\tapemonitor.log" -value "Windows was not able to see the other connected device"
-    add-content -path "$logpath\tapemonitor.log" -value "The script will now terminate, please provide the log to Veeam"
-    explorer.exe "$logpath"
-    exit
+$TestCount = Get-DeviceCount
+	if ($WorkingCount -gt $TestCount){
+		Write-Log -LogText "One of the connected Devices has fallen offline"
+		Write-Log -LogText "Windows was not able to see the device via the gwmi commands"
+		Write-Log -LogText "The script will now terminate, please add this log to your case"
+		explorer.exe $logpath
+		exit
 	}
 	elseif($i % 6 -eq "0"){
-		add-content -path "$logpath\tapemonitor.log"	-value (Get-Date)
-		add-content -path "$logpath\tapemonitor.log"	-value "Both Libraries Connected"
+		Write-Log -LogText "All Devices Connected"
 		Start-sleep 10
 		}
     else {
