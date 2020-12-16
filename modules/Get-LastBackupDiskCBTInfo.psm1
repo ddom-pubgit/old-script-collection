3ce6375a10e0e9ac6167493b9cb74784e855a2df
ddom <david.domask@veeam.com>
2020-12-16 09:37:17 +0000

    Update Get-LastBackupDiskCBTInfo.psm1

--- modules/Get-LastBackupDiskCBTInfo.psm1
+++ modules/Get-LastBackupDiskCBTInfo.psm1
@@ -2,6 +2,6 @@ function Get-LastBackupDiskCBTInfo {
 	param (
 		[object[]]$Backup
 		)
-		$LastBackupDiskCBTInfo = ($backup.GetLastOibs()|Where-Object {$_.VMname -eq "$VMname"})AuxData.DisksInfos
+		$LastBackupDiskCBTInfo = ($backup.GetLastOibs()|Where-Object {$_.VMname -eq "$VMname"}).AuxData.DisksInfos
 		return $LastBackupDiskCBTInfo
 		}
