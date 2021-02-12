44d055764bd9ee6376457eab4cc61cc839bdf40d
ddom <david.domask@veeam.com>
2021-02-12 12:53:18 +0300

    Updated script for v11, split to two separate scripts for now to reduce confusion. Modules have been updated.
    Get-StoragesPathsAndBlocksizeFromBackup has been renamed into two:
    
    Get-StoragesPathsAndBlocksizeFromBackup-v10
    Get-StoragesPathsAndBlocksizeFromBackup-v11
    
    Change was that ScaleOutRepository objects no longer have FindExtentRepo as a method.
    FindExtentRepo needs to be updated in scripts to FindRepositoryForExistingStorage

--- IsFastClonePossibleForBackup-v11-only.ps1
+++ IsFastClonePossibleForBackup-v11-only.ps1
@@ -1,3 +1,20 @@
+<# script returns the blocksize of Storages for a Backup and also the Repository Information.
+Works for Scaleout Repos and also Local Windows/Linux (should work for XFS)
+Should work with Agent Backups now
+Not Tested with CloudConnect Repos
+
+#>
+
+<#Todo:
+- Add validation rules for why Fastclone is not posssible on a given repository
+#>
+
+param (
+    [string]$BackupName
+    )
+
+Add-PSSnapin VeeamPSSnapin -ErrorAction SilentlyContinue
+
 function Get-WindowsRepositoryInfo {
     param(
         [string[]]$RepositoryID
@@ -59,31 +76,8 @@ function Get-RepositoryTypeAndInfo {
 	$RepositoryData += (Show-IsFastClonePossibleandClusterSize -BackupRepositoryInfo $RepoInfo)
 	return	$RepositoryData
 } 	
-function Get-StoragesPathsAndBlocksizeFromBackup-v10 {
-	param(
-		[Parameter(Mandatory=$true, Position=0)]
-		[Object[]]$Backup
-	)
-	if($Backup.JobType -eq 'EpAgentBackup' -or $Backup.Jobtype -eq 'SimpleBackupCopyPolicy'){
-		$Storages = $Backup[0].GetallChildrenStorages() | Sort-Object -Property PartialPath, CreationTime -Descending
-	} else {
-		$Storages = $Backup[0].GetAllStorages() | Sort-Object -Property PartialPath, CreationTime -Descending
-	}
-	$Repository = $Backup.FindRepository()[0]
-	$StoragePathsandBlocksize  = @()
-	if($Repository.Type -eq "ExtendableRepository"){
-		foreach($Storage in $Storages){
-			$Extent = $Repository.FindExtentRepo($Storage.Id)
-			$job = $Backup.FindJob()
-			$StoragePathsandBlocksize += New-Object -TypeName psobject -Property @{Extent=$Extent.Name;Path=$($Extent.Path.ToString(),$job.Name.ToString(),$Storage.filepath -join "\");BlockSize=$Storage.BlockAlignmentSize;CreationTime=$Storage.CreationTime}
-			}
-	} else {
-		$StoragePathsandBlocksize += $Storages | Sort-Object -Property PartialPath, CreationTime -Descending | Select-Object -Property PartialPath,BlockAlignmentSize
-	}
-	return $StoragePathsandBlocksize
-}
 
-function Get-StoragesPathsAndBlocksizeFromBackup-v11 {
+function Get-StoragesPathsAndBlocksizeFromBackup {
 	param(
 		[Parameter(Mandatory=$true, Position=0)]
 		[Object[]]$Backup
@@ -106,3 +100,24 @@ function Get-StoragesPathsAndBlocksizeFromBackup-v11 {
 	}
 	return $StoragePathsandBlocksize
 }
+
+
+$Backup = Get-VBRBackup -Name $BackupName
+$TargetRepository = $Backup.FindRepository()[0] #Jobs with Offloads in Copy Mode will return multiple entries from FindRepository() but both will be PerformanceTier. Safe to simply set the first
+$StoragesStats = Get-StoragesPathsAndBlocksizeFromBackup -Backup $Backup
+$RepositoryData = @()
+
+If($TargetRepository.Type -eq 'ExtendableRepository'){
+		$ExtentList = Get-ExtentFromScaleout -ScaleoutRepoName $TargetRepository.Name
+		Foreach($Extent in $ExtentList){
+			Get-RepositoryTypeAndInfo -RepositoryToCheck $Extent
+		}
+	
+	$RepositoryData | Out-Host
+	$StoragesStats| Select-Object -Property Extent,Path,Blocksize | Out-Host
+} else {
+	Get-RepositoryTypeAndInfo -RepositoryToCheck $TargetRepository
+	$RepositoryData | Out-Host
+	$StoragesStats | Out-Host
+}
+	
