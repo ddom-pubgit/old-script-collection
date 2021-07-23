e3ee3d299031296729798b24aa33133f26d23e39
ddom <david.domask@veeam.com>
2021-07-23 19:25:45 +0300

    Tape Monitor Update:
    - iscsi monitoring support via the -Iscsi parameter
    - Count all devices with -Allcheck (Drives + Changer are checked independently by default, this will count all)
    - Event logs can be collected with -Events parameter

--- tape-monitor-script/tapemonitor-v2.ps1
+++ tape-monitor-script/tapemonitor-v2.ps1
@@ -5,14 +5,14 @@ Every minute, it will log if All devices are successfully connected.
 #>
 
 param (
-    [switch]$AllCheck = $false,
     [switch]$DriveCheck = $false,
-	[switch]$Iscsi = $false,
-	[switch]$Events = $false
+	[switch]$iscsi = $false,
+	[switch]$events = $false
     )
 #Configuration variables. Adjust these as necessary for the environment
-$logpath = "C:\temp\TapeMonitorLogging"
-$logfilefullpath = "$logpath\TapeMonitorLog.log"
+
+$logfilefullpath = "C:\temp\TapeMonitorLog.log"
+$logpath = "C:\temp"
 $Duration = "60480" #Value should be (total duration in seconds)/10. Default is 1 week. The script should not be aggressive, so feel free to run it for weeks even
 
 function Write-Log {
@@ -24,57 +24,44 @@ function Write-Log {
     }
 
 function Get-DeviceCount {
-    $Drives = gwmi -Class Win32_TapeDrive
-    $Changers = gwmi -Namespace root\wmi -Class mschangerparameters
-    if($AllCheck){
-       $DeviceNum = $Drives.Count + $Changers.Count
-    }
-    elseif($DriveCheck){
+	if($DriveCheck){
+		$Drives = gwmi -Class Win32_TapeDrive
 		$DeviceNum = $Drives.Count
 	} else {
+		$Changers = gwmi -Namespace root\wmi -Class mschangerparameters
 		$DeviceNum = $Changers.Count
 	}
 return $DeviceNum
 }
 
-function Get-IscsiInformation {
-	$iscsiTargets = Get-IscsiTarget | Where-Object{$_.NodeAddress -like  "*vtl*" -or $_.NodeAddress -like "*amazon*"}
-	Return $iscsiTargets
-}
-
 $WorkingCount = Get-DeviceCount
-if($iscsi){$WorkingIscsi = Get-IscsiInformation}
-if(!(Test-Path $logpath)){New-Item -ItemType Directory -Path $logpath | Out-Null}
+if(!(Test-Path $logpath)){New-Item -ItemType Directory -Path $logpath}
 Write-Log -LogText "Baseline Value: $WorkingCount"
-Write-Log -LogText ((Get-WmiObject Win32_PnPSignedDriver | Where-Object {($_.DEVICECLASS -eq "TapeDrive")} | Select-Object -Property Description, Location) | Out-String)
-Write-Log -LogText ((gwmi -Namespace root\wmi -Class mschangerparameters) | Out-String)
-Write-Log -LogText ($WorkingIscsi | Out-String)
-Write-Host "Script has started monitoring. It will auto-stop once an issue is detected or after 1 week"
+Write-Log -LogText (Get-WmiObject Win32_PnPSignedDriver | Where-Object {($_.DEVICECLASS -eq "TapeDrive")} | Select-Object -Property Description, Location)
+Write-Log -LogText (gwmi -Namespace root\wmi -Class mschangerparameters)
+
 for($i=0;$i -lt "$Duration"; $i++){
 $TestCount = Get-DeviceCount
 	if ($WorkingCount -gt $TestCount){
+		if($iscsi){
+			
 		Write-Log -LogText "One of the connected Devices has fallen offline"
 		Write-Log -LogText "Windows was not able to see the device via the gwmi commands"
 		Write-Log -LogText "The script will now terminate, please add this log to your case"
 		Write-Log -LogText "Current Value: $TestCount"
-		Write-Log -LogText ((Get-WmiObject Win32_PnPSignedDriver | Where-Object {($_.DEVICECLASS -eq "TapeDrive")} | Select-Object -Property Description, Location) | Out-String)
-		Write-Log -LogText ((gwmi -Namespace root\wmi -Class mschangerparameters) | Out-String)
-		if($iscsi){
-			Write-Log -LogText "Current iscsi Target Status:"
-			$CurrentIscsi = Get-IscsiInformation
-			Write-Log -LogText ($CurrentIscsi | Out-String)
-		}
+		Write-Log -LogText (Get-WmiObject Win32_PnPSignedDriver | Where-Object {($_.DEVICECLASS -eq "TapeDrive")} | Select-Object -Property Description, Location)
+		Write-Log -LogText (gwmi -Namespace root\wmi -Class mschangerparameters)s
 		if($events){
 				Write-Host -ForegroundColor Yellow "Getting Windows Events. This may take up to several minutes. For very active machines, it may take longer"
 				[System.IO.Directory]::CreateDirectory("$logpath\Events") | Out-Null
 				$Application=(Get-WmiObject -Class Win32_NTEventlogFile -Filter "LogFIleName='Application'").BackupEventLog("$logpath\Events\Application.evtx")
 				$System=(Get-WmiObject -Class Win32_NTEventlogFile -Filter "LogFIleName='System'").BackupEventLog("$logpath\Events\System.evtx")
 				#Events in CSV are only for the last 2 days.
-				$ApplicationCSV=Get-EventLog -LogName Application -After (Get-Date).AddDays(-2) |  Select-Object eventID, entrytype, source, message, timegenerated | Export-Csv $logpath\Events\Application.csv
-				$SystemCSV=Get-EventLog -LogName System -After (Get-Date).AddDays(-2) |  Select-Object eventID, entrytype, source, message, timegenerated | Export-Csv $logpath\Events\System.csv
+				$ApplicationCSV=Get-EventLog -LogName Application -After (Get-Date).AddDays(-2) |  select eventID, entrytype, source, message, timegenerated | Export-Csv $logpath\Events\Application.csv
+				$SystemCSV=Get-EventLog -LogName System -After (Get-Date).AddDays(-2) |  select eventID, entrytype, source, message, timegenerated | Export-Csv $logpath\Events\System.csv
 				Write-Host -ForegroundColor Yellow "Done."
 		}	
-		explorer.exe $logpath
+		explorer.exe $logfilefullpath
 		exit
 	}
 	elseif($i % 6 -eq "0"){
